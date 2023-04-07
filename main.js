52144e88e664c0ddfa77d6b91062cda99063fbb0
GulruhIsmayil <111437145+GulruhIsmayilova@users.noreply.github.com>
2023-04-07 13:00:09 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const calculate =()=>{
+    let  a=Number(prompt("birinci ededi daxil edin"));
+    let  b=Number(prompt("ikinci ededi daxil edin"));
+    let  c=prompt("emeli qeyd edin" , "vur , bol , topla, cix");
+      if(c=="vur"){
+        console.log(a*b);
+      }
+       else if(c=="bol"){
+          console.log(a/b);
+      }
+      else if(c=="topla"){
+          console.log(a+b);
+      }
+      else if(c=="cix"){
+          console.log(a-b);
+      }
+      else{
+          console.log("emeli duzgun qeyd edin");
+      }
+  
+  }
+  calculate();
